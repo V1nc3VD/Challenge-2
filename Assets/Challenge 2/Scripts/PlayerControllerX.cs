da9b9fe42a83b7ee7524d8d4dad4b1f062480c79
Vince van Doorn <328388@student.mboutrecht.nl>
2019-12-09 23:55:34 +0100

    Af

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float fireRate = 2.0f;
+    private float nextFire = 0.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        //Telt de verstreken tijd van het spel en doet daar telkens bij het gebruiken firerate bij in de float Nextfire. 
+        //Dit zorgt ervoor dat er telkens 2 seconde wachttijd is.
+        if (Input.GetKeyDown(KeyCode.Space) &&  Time.time > nextFire )
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 2.0f;
+
+
+
+    private void Update()
+    {
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnRandomBall",  startDelay);
+    }
+
+    // Spawn random balls
+    void SpawnRandomBall ()
+    {
+
+        //zorgt ervoor dat de ballen op een random tijd spawnen
+        float spawnInterval = Random.Range(3.0f, 5.0f);
+        
+        //random x position at top of play area
+        int index = Random.Range(0, ballPrefabs.Length);
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[index].transform.rotation);
+        Invoke("SpawnRandomBall",  spawnInterval);
+    }
+    
+    
+}
